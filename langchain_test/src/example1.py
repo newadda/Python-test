29ff71b59c57a63e91ad93c0340e1dd45e33aba2
shh <newadda@naver.com>
2024-04-26 12:58:58 +0900

    테스트중

--- langchain_test/src/example1.py
+++ langchain_test/src/example1.py
@@ -10,7 +10,7 @@ from langchain.vectorstores.utils import filter_complex_metadata
 from langchain_community.llms import Ollama
 
 from langchain_community.embeddings import OllamaEmbeddings
-
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 class ChatPDF:
     vector_store = None
@@ -18,14 +18,15 @@ class ChatPDF:
     chain = None
 
     def __init__(self):
-        self.model = ChatOllama(model="mistral")
+        self.model = ChatOllama(model="llama3")
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
         self.prompt = PromptTemplate.from_template(
             """
-            <s> [INST] 당신은 질문에 답변하는 답변자입니다. 참고내용을 사용하여 질문에 답하세요. 답을 모르면 모른다고 세 문장으로 표현하세요. 최대한 간결하게 답변하세요. [/INST] </s> 
-            [INST] 질문: {question} 
-            참고내용: {context} 
-            답변: [/INST]
+            [INST] 당신은 질문에 답변하는 사람입니다. 아래의 참고내용을 사용하여 질문에 답하세요. 답을 모르면 모른다고 세 문장으로 표현하세요. 최대한 간결하게 답변하세요. 한국어로 말해주세요. [/INST]
+            
+            {context} 
+
+            {question} 
             """
         )
     
@@ -34,10 +35,15 @@ class ChatPDF:
         chunks = self.text_splitter.split_documents(docs)
         chunks = filter_complex_metadata(chunks)
 
-        ##vector_store = Chroma.from_documents(documents=chunks, embedding=FastEmbedEmbeddings())
-        ollama_emb = OllamaEmbeddings(model="mistral",)
-        vector_store = Chroma.from_documents(documents=chunks, embedding=ollama_emb)
-        
+        #---------------------------------------------------------------
+        hf = HuggingFaceEmbeddings(model_name='jhgan/ko-sroberta-multitask')
+        vector_store = Chroma.from_documents(documents=chunks, embedding=hf)
+        #---------------------------------------------------------------
+        #vector_store = Chroma.from_documents(documents=chunks, embedding=FastEmbedEmbeddings())
+        #--------------
+        ##ollama_emb = OllamaEmbeddings(model="llama3",)
+        ##vector_store = Chroma.from_documents(documents=chunks, embedding=ollama_emb)
+        #-------------
         self.retriever = vector_store.as_retriever(
             search_type="similarity_score_threshold",
             search_kwargs={
