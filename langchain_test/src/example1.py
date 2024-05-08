df5fe8d673fd95a8fe0b2752c5e4db2ceb175945
shh <newadda@naver.com>
2024-05-08 16:35:39 +0900

    테스트중

--- langchain_test/src/example1.py
+++ langchain_test/src/example1.py
@@ -5,7 +5,10 @@ from langchain.schema.output_parser import StrOutputParser
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter ,CharacterTextSplitter
 from langchain.schema.runnable import RunnablePassthrough
-from langchain.prompts import PromptTemplate,ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain.prompts import PromptTemplate,ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
+
+
+
 
 from langchain.vectorstores.utils import filter_complex_metadata
 from langchain_community.llms import Ollama
@@ -13,25 +16,42 @@ from langchain_community.llms import Ollama
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
+
+####=========== 메모리
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationSummaryBufferMemory
+
+####=========== 로그
+from langchain_core.callbacks import FileCallbackHandler, StdOutCallbackHandler
+from langchain.globals import set_verbose
+set_verbose(True)
+
 class ChatPDF:
     vector_store = None
     retriever = None
     chain = None
 
     def __init__(self):
-        self.model = ChatOllama(model="llama3")
+        
+        ##### === Model 셋팅
+        self.model = ChatOllama(model='llama3-ko-8b-q4')
+        
+        ##### === Splitter 셋팅
         #self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
         self.text_splitter = CharacterTextSplitter(separator="\n",
-                                        chunk_size=1000,
-                                        chunk_overlap=200,
+                                        chunk_size=200,
+                                        chunk_overlap=100,
                                         length_function=len) 
+        
+        ##### === Prompt 셋팅
+        ### 기본 셋팅
         system_template = """
-        "You are a helpful assistant. 한국어로 번역해서 답해주세요."""
+        당신은 질문에 답변하는 한국어 AI봇입니다.
+        """
         system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
 
-
-        # (b) Human Message
-        human_template = """
+        human_template = r"""
                     <요구사항> 으로 시작하는 태그의 내용은 너가 지켜야 할 사항들이야. 이것을 지켜줘.
                     
                     <요구사항>
@@ -48,20 +68,51 @@ class ChatPDF:
                     
                     
                     <질문> {question} </질문>
+                   
                     """
+                    
+
         human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 
         self.prompt = ChatPromptTemplate.from_messages(
             [
-                system_message_prompt,human_message_prompt
+                system_message_prompt, human_message_prompt
             ]
         )
-    
+        
+        ### 메모리 테스트용 template
+        system_template = r"""
+        당신은 질문에 답변하는 한국어 AI봇입니다. <학습>으로 시작하고 </학습>으로 끝나는 내용을 기반으로 답하세요.
+        """
+        system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+
+        human_template = """
+                    {question} 
+                    """
+                    
+        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+        self.prompt = ChatPromptTemplate.from_messages(
+            [
+                system_message_prompt,MessagesPlaceholder(variable_name="chat_history"), human_message_prompt
+            ]
+        )
+        
+        # 메모리 셋팅
+        self.memory = ConversationSummaryBufferMemory(
+            llm=self.model,
+            max_token_limit=80,
+            memory_key="chat_history",
+            return_messages=True,
+        )
+
+        
     def ingest(self, pdf_file_path: str):
         docs = PyPDFLoader(file_path=pdf_file_path).load()
         chunks = self.text_splitter.split_documents(docs)
         chunks = filter_complex_metadata(chunks)
-
+        
+        
         #---------------------------------------------------------------
         hf = HuggingFaceEmbeddings(model_name='jhgan/ko-sroberta-multitask')
         vector_store = Chroma.from_documents(documents=chunks, embedding=hf)
@@ -83,6 +134,56 @@ class ChatPDF:
                       | self.prompt
                       | self.model
                       | StrOutputParser())
+        ### 메모리
+        handler_2 = StdOutCallbackHandler()
+        self.chain = ({"context": self.retriever, "question": RunnablePassthrough()}
+                      | RunnablePassthrough.assign(chat_history=self.load_memory)
+                      | self.prompt
+                      | self.model
+                      
+                      | StrOutputParser()
+                      )     
+        
+        ### 미리 교육
+        self.preLearning(chunks)   
+    
+     ### 메모리
+    def load_memory(self,input):
+        print(input)
+        return self.memory.load_memory_variables({})["chat_history"]    
+    
+    def preLearning(self,chunks):
+        q = "너는 <학습> 으로 시작하고 </학습> 으로 끝날 때 까지 해당 내용을 학습해야해."
+        result= self.chain.invoke(q)
+        self.memory.save_context(
+                        {"input": q},
+                        {"output": result},
+                    )
+        q = "<학습>"
+        self.chain.invoke(q)
+        self.memory.save_context(
+                        {"input": q},
+                        {"output": result},
+                    )
+        
+        for _s in chunks:
+            print("\n문장-----------------\n")
+            print(_s.page_content)
+            if _s:
+                result=self.chain.invoke(_s.page_content)
+                self.memory.save_context(
+                        {"input": _s.page_content},
+                        {"output": result},
+                    )
+                
+        q = "<학습>"
+        self.chain.invoke(q)
+        self.memory.save_context(
+                        {"input": q},
+                        {"output": result},
+                    )
+        
+        pass
 
     def ask(self, query: str):
         if not self.chain:
@@ -93,7 +194,13 @@ class ChatPDF:
         prompt = self.prompt.format(context=context_text, question=query)
         return self.model.predict(prompt)
         '''
-        return self.chain.invoke(query)
+        result=self.chain.invoke(query)
+        self.memory.save_context(
+                        {"input": query},
+                        {"output": result},
+                    )
+        
+        return result
 
     def clear(self):
         self.vector_store = None
